db725440099369bf7c2c25581adbc0e67d6bff99
Mitch Harris <mitchharris@live.ca>
2021-01-29 18:11:50 -0800

    create api function to test api call via env secret

---
+++ R/getRecallByNumber.R
@@ -0,0 +1,23 @@
+# where does this go?
+library(httr)
+
+#' get_recall_by_number
+#'
+#' @param recall_number An integer
+#'
+#' @return url response
+#' @export
+#'
+#' @examples
+#' get_recall_by_number(1977044)
+get_recall_by_number <- function(recall_number) {
+
+    # format the url string
+    u <- "https://vrdb-tc-apicast-production.api.canada.ca/eng/vehicle-recall-database/v1/recall/recall-number/"
+    u <- paste(u, toString(recall_number), sep="")
+
+    # query the api
+    r <- GET(u, add_headers("user-key"=Sys.getenv("VRD_API")))
+    c <- content(r)
+    return(c)
+}
