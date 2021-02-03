2242412a5fb68f443e3d1bc750cc2c2cf2e70918
Mitch Harris <mitchharris@live.ca>
2021-02-03 11:39:43 -0800

    add new test file

--- tests/testthat/test-rangeyear.R
+++ tests/testthat/test-rangeyear.R
@@ -3,7 +3,6 @@ test_that("fbind() binds factor (or character)", {
     x_fact <- factor(x)
     y <- c("c", "d")
     z <- factor(c("a", "b", "c", "d"))
-
     expect_identical(fbind(x, y), z)
     expect_identical(fbind(x_fact, y), z)
 })
