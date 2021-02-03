090b530c178e227a72f878fd6ea8eb1826f1c5dd
Mitch Harris <mitchharris@live.ca>
2021-02-03 12:37:51 -0800

    add test for api key check

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("bad api key correctly fails", {
+
+  make <- "Mazda"
+  working_api <- Sys.getenv("VRD_API")
+  Sys.setenv(VRD_API = "test")
+  url_ <- "https://vrdb-tc-apicast-production.api.canada.ca/eng/vehicle-recall-database/v1/recall/make-name/"
+  url_ <- paste(url_, toString(make), "/count", sep = "")
+  expect_error(call_vrd_api(url_,make))
+  Sys.setenv(VRD_API = working_api)
+
 })
