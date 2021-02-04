3c61f5ebcb8b5430a995262ceed7a812ec35298c
Mitch Harris <mitchharris@live.ca>
2021-02-03 16:28:17 -0800

    use simplier check()

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -8,6 +8,6 @@ test_that("bad api key correctly fails", {
 test_that("call_rd_api() returns the correct structure",{
     # uses an example from the site with out an api key
     test_url <- "http://data.tc.gc.ca/v1.3/api/eng/vehicle-recall-database/recall/make-name/volkswagen/count?format=json "
-    response <- call_vrd_api(test_url, "")
+    response <- call_vrd_api(test_url, "", api_key="test")
     expect_type(response, "list")
 })
