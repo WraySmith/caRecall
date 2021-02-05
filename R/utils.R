77efee3442b22038ee62724e89c43e32401712fa
RyKoe <ryan.koenig@hotmail.ca>
2021-02-05 12:13:01 -0800

    Fixed row numbering in dataframe

--- R/utils.R
+++ R/utils.R
@@ -110,7 +110,6 @@ clean_vrd_api <- function(api_output){
     df2 <- df1[, grep("Literal", colnames(df1))]
 
     # transposes dataframe
-    df3 <- as.data.frame(t(df2), row.names = FALSE)
 
     # adds back the correct column names
     colnames(df3) <- df1$Name
