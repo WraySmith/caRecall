8db55768f4c297a04cf7c6eda95fd1c5820f7327
Nathan Smith <nwraysmith@gmail.com>
2021-02-06 13:48:51 -0800

    add tests for get_vrd_api and call_vrd_api

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 test_that("API key is retreived", {
   expect_type(get_vrd_key(), 'character')
 })
@@ -11,6 +12,22 @@ test_that("Bad API key correctly fails", {
 })
 
 test_that("API call returns correct structure", {
+=======
+context("Helper Functions")
+
+test_that("API key is retreived", {
+  expect_type(get_vrd_key(), 'character')
+})
+
+test_that("Bad API key correctly fails", {
+  # valid url
+  url_test <- 'https://vrdb-tc-apicast-production.api.canada.ca/eng/vehicle-recall-database/v1/recall/make-name/ford'
+  query_test <- 'ford'
+  bad_key <- 'test'
+  expect_error(call_vrd_api(url_test, query = query_test, api_key = bad_key))
+})
+
+test_that("API call returns correctly",{
     url_test <- "https://vrdb-tc-apicast-production.api.canada.ca/eng/vehicle-recall-database/v1/recall/make-name/ford"
     query_test <- 'ford'
     response <- call_vrd_api(url_test, query = query_test)
@@ -19,6 +36,17 @@ test_that("API call returns correct structure", {
     expect_equal(response$response$status_code, 200)
 })
 
+test_that("API call returns correctly",{
+>>>>>>> 6b2e748 (add tests for get_vrd_api and call_vrd_api)
+    url_test <- "https://vrdb-tc-apicast-production.api.canada.ca/eng/vehicle-recall-database/v1/recall/make-name/ford"
+    query_test <- 'ford'
+    response <- call_vrd_api(url_test, query = query_test)
+    expect_s3_class(response, 'vrd_api', exact = FALSE)
+    expect_type(response$response, 'list')
+    expect_equal(response$response$status_code, 200)
+})
+
+<<<<<<< HEAD
 test_that("Clean API returns formatted dataframe", {
     url_test1 <- "https://vrdb-tc-apicast-production.api.canada.ca/eng/vehicle-recall-database/v1/recall/make-name/ford"
     query_test1 <- 'ford'
@@ -32,3 +60,5 @@ test_that("Clean API returns formatted dataframe", {
     expect_equal(nrow(clean_vrd_api(response2)), 1)
 })
 
+=======
+>>>>>>> 6b2e748 (add tests for get_vrd_api and call_vrd_api)
