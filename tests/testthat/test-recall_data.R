79adae5ab6be63ead7817df2e4172ab10d2f86a3
Mitch Harris <mitchharris@live.ca>
2021-02-06 18:35:09 -0800

    fix typo

--- tests/testthat/test-recall_data.R
+++ tests/testthat/test-recall_data.R
@@ -79,9 +79,9 @@ test_that("recall_by_model has the correct shape",{
                          "character", "integer",   "Date")
     returned_dtypes <- as.vector(unlist(sapply(result, class)))
     expect_equal(returned_dtypes, expected_dtypes)
-    result <- recall_by_make('Nissan', start_year = 1980)
+    result <- recall_by_model('Nissan', start_year = 1980)
     expect_gt(nrow(result), 0)
-    result <- recall_by_make('Nissan', end_year = 1980)
+    result <- recall_by_model('Nissan', end_year = 1980)
     expect_gt(nrow(result), 0)
 })
 
