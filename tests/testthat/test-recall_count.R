1c6db1a2af0b2ccc9e1536f54e28fba5dea5d719
RyKoe <ryan.koenig@hotmail.ca>
2021-02-06 22:20:52 -0800

    Updated proper count function call in test

--- tests/testthat/test-recall_count.R
+++ tests/testthat/test-recall_count.R
@@ -1,7 +1,7 @@
 test_that("count_recall_by_make returns expected value", {
     query <- count_recall_by_make("Nissan", start_year = 1995, end_year = 1998)
     expect_type(query, "list")
-    expect_equal(query[[1]], 43)
+    expect_equal(query[[1]], 43L)
     expect_equal(nrow(query), 1)
     expect_equal(colnames(query), "Result Count")
 })
@@ -16,24 +16,37 @@ test_that("count_recall_by_make shape with default limits", {
 test_that("count_recall_by_make with manufacturer", {
     query <- count_recall_by_make("Nissan", manufacturer = TRUE, start_year = 1995, end_year = 1998)
     expect_type(query, "list")
-    expect_equal(query[[1]], 59)
+    expect_equal(query[[1]], 59L)
     expect_equal(nrow(query), 1)
     expect_equal(colnames(query), "Result Count")
 })
 
 test_that("count_recall_by_model returns expected value", {
-    query <- count_recall_by_make("Altima", start_year = 1995, end_year = 1998)
+    query <- count_recall_by_model("Altima", start_year = 1995, end_year = 1998)
     expect_type(query, "list")
-    expect_equal(query[[1]], 43)
+    expect_equal(query[[1]], 2L)
     expect_equal(nrow(query), 1)
     expect_equal(colnames(query), "Result Count")
 })
 
 test_that("count_recall_by_model shape with default limits", {
-    query <- count_recall_by_make("Altima")
+    query <- count_recall_by_model("Altima")
     expect_type(query, "list")
     expect_equal(nrow(query),1)
     expect_equal(colnames(query), "Result Count")
 })
 
+test_that("count_recall_by_years returns expected value", {
+    query <- count_recall_by_years(start_year = 1995, end_year = 1998)
+    expect_type(query, "list")
+    expect_equal(query[[1]], 2L)
+    expect_equal(nrow(query), 1)
+    expect_equal(colnames(query), "Result Count")
+})
 
+test_that("count_recall_by_years with default limits", {
+    query <- count_recall_by_make("Altima")
+    expect_type(query, "list")
+    expect_equal(nrow(query),1)
+    expect_equal(colnames(query), "Result Count")
+})
