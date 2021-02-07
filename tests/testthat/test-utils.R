575dfbe66659084bcb206c8474cb2d201eca3e85
Nathan Smith <nwraysmith@gmail.com>
2021-02-06 17:21:56 -0800

    add utils.R unittests

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,11 +1,44 @@
-test_that("bad api key correctly fails", {
-  url_ <- "https://vrdb-tc-apicast-production.api.canada.ca/eng/vehicle-recall-database/v1/recall/make-name/ford"
-  expect_error(call_vrd_api(url_, 'ford', api_key="test"))
+test_that("API key is retreived", {
+  expect_type(get_vrd_key(), 'character')
 })
 
-test_that("call_rd_api() returns the correct structure",{
-    # uses an example from the site with out an api key
-    test_url <- "https://vrdb-tc-apicast-production.api.canada.ca/eng/vehicle-recall-database/v1/recall/make-name/ford"
-    response <- call_vrd_api(test_url, "ford")
-    expect_type(response, "list")
+test_that("API call returns correct structure", {
+
+test_that("Bad API key correctly fails", {
+  # valid url
+  url_test <- 'https://vrdb-tc-apicast-production.api.canada.ca/eng/vehicle-recall-database/v1/recall/make-name/ford'
+  query_test <- 'ford'
+  bad_key <- 'test'
+  expect_error(call_vrd_api(url_test, query = query_test, api_key = bad_key))
+})
+
+test_that("API call returns correct structure", {
+    url_test <- "https://vrdb-tc-apicast-production.api.canada.ca/eng/vehicle-recall-database/v1/recall/make-name/ford"
+    query_test <- 'ford'
+    response <- call_vrd_api(url_test, query = query_test)
+    expect_s3_class(response, 'vrd_api', exact = FALSE)
+    expect_type(response$response, 'list')
+    expect_equal(response$response$status_code, 200)
+})
+
+test_that("Clean API returns formatted dataframe", {
+    # setup for one of the output structures
+    # used for all functions except `recall_details()`
+    url_test1 <- "https://vrdb-tc-apicast-production.api.canada.ca/eng/vehicle-recall-database/v1/recall/make-name/ford"
+    query_test1 <- 'ford'
+    response1 <- call_vrd_api(url_test1, query = query_test1, limit = 25)
+    coltype1 <- c(rep('character', 4), 'integer', 'double')
+    # setup for other output structures, used by `recall_detils()`
+    url_test2 <- "https://vrdb-tc-apicast-production.api.canada.ca/eng/vehicle-recall-database/v1/recall-summary/recall-number/1977044"
+    query_test2 <- '1977045'
+    response2 <- call_vrd_api(url_test2, query = query_test2)
+    coltype2 <- c(rep('character', 13), 'integer', 'double')
+    # tests for first structure
+    expect_s3_class(clean_vrd_api(response1), 'data.frame')
+    expect_equal(nrow(clean_vrd_api(response1)), 25)
+    expect_equal(as.vector(sapply(clean_vrd_api(response1), typeof)), coltype1)
+    # tests for second structure
+    expect_s3_class(clean_vrd_api(response2), 'data.frame')
+    expect_equal(nrow(clean_vrd_api(response2)), 1)
+    expect_equal(as.vector(sapply(clean_vrd_api(response2), typeof)), coltype2)
 })
