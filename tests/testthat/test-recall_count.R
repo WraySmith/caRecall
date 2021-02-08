027e92ef98e58820988124ad0d0ff39f17e4e0f5
RyKoe <ryan.koenig@hotmail.ca>
2021-02-07 19:52:40 -0800

    Removed calls without year

--- tests/testthat/test-recall_count.R
+++ tests/testthat/test-recall_count.R
@@ -6,14 +6,6 @@ test_that("count_recall_by_make returns expected value", {
     expect_equal(colnames(query), "Result Count")
 })
 
-test_that("count_recall_by_make list with default limits", {
-    query <- count_recall_by_make(c("Nissan","Honda"))
-    expect_type(query, "list")
-    expect_type(query[[1]], "integer")
-    expect_equal(nrow(query),1)
-    expect_equal(colnames(query), "Result Count")
-})
-
 test_that("count_recall_by_make with manufacturer", {
     query <- count_recall_by_make("Nissan", manufacturer = TRUE, start_year = 1995, end_year = 1998)
     expect_type(query, "list")
@@ -30,26 +22,10 @@ test_that("count_recall_by_model returns expected value", {
     expect_equal(colnames(query), "Result Count")
 })
 
-test_that("count_recall_by_model list with default limits", {
-    query <- count_recall_by_model(c("Altima","Civic"))
-    expect_type(query, "list")
-    expect_type(query[[1]], "integer")
-    expect_equal(nrow(query),1)
-    expect_equal(colnames(query), "Result Count")
-})
-
 test_that("count_recall_by_years returns expected value", {
     query <- count_recall_by_years(start_year = 1995, end_year = 1998)
     expect_type(query, "list")
     expect_equal(query[[1]], 5597L)
     expect_equal(nrow(query), 1)
-    expect_equal(colnames(query), "Result Count")
-})
-
-test_that("count_recall_by_years with default limits", {
-    query <- count_recall_by_years()
-    expect_type(query, "list")
-    expect_type(query[[1]], "integer")
-    expect_equal(nrow(query),1)
     expect_equal(colnames(query), "Result Count")
 })
